3853d7140317c4bdfa758e77b037cfdcef1913cb
Elijah <ellayab804@gmail.com>
2025-09-22 01:20:57 +0300

    header and navigation bar completed

--- interfaces/index.ts
+++ interfaces/index.ts
@@ -1,7 +1,38 @@
-interface CardProps {
-
+export interface CardProps {
+    name: string;
 }
 
-interface ButtonProps {
-    
+export interface ButtonProps {
+    label: string;
+    onClick: () => void;
+}
+
+export interface LayoutProps {
+  children: React.ReactNode;
+}
+
+export interface PropertyProps {
+  name: string;
+  address: {
+    state: string;
+    city: string;
+    country: string;
+  };
+  rating: number;
+  category: string[];
+  price: number;
+  offers: {
+    bed: string;
+    shower: string;
+    occupants: string;
+  };
+  image: string;
+  discount: string;
+}
+
+export interface CategoryIconProps {
+  icon: string;
+  label: string; 
+  active?: boolean;
+  onClick?: () => void;
 }
